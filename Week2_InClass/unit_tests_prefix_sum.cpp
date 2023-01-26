6e2f7d10c121ad4a5908621fec133b5c07ff64e4
GodOrGovern <willkeenan12@gmail.com>
2023-01-26 12:37:43 -0500

    Add unit tests

--- Week2_InClass/unit_tests_prefix_sum.cpp
+++ Week2_InClass/unit_tests_prefix_sum.cpp
@@ -14,7 +14,10 @@ int nums_2[] = {35};
 int nums_3[] = {2,-3,3,2};
 int nums_4[] = {1,1,-1,1};
 int nums_5[] = {-1,-1,-1,-1,1,1};
-
+int nums_6[] = {-1,-1,-1,-1,-1,6};
+int nums_7[] = {0,0,0,0,0};
+int nums_8[] = {1,-1,1,-1,1,-1};
+int nums_9[] = {-1,1,-1,1,-1,1};
 
 TEST_CASE("Test week1/2 functions") {
   SUBCASE("non-negative prefix sums") {
@@ -24,6 +27,10 @@ TEST_CASE("Test week1/2 functions") {
       CHECK(non_negative_sum(nums_3, ARRAY_SIZE(nums_3)) == false);
       CHECK(non_negative_sum(nums_4, ARRAY_SIZE(nums_4)) == true);
       CHECK(non_negative_sum(nums_5, ARRAY_SIZE(nums_5)) == false);
+      CHECK(non_negative_sum(nums_6, ARRAY_SIZE(nums_6)) == false);
+      CHECK(non_negative_sum(nums_7, ARRAY_SIZE(nums_7)) == true);
+      CHECK(non_negative_sum(nums_8, ARRAY_SIZE(nums_8)) == true);
+      CHECK(non_negative_sum(nums_9, ARRAY_SIZE(nums_9)) == false);
   };
   SUBCASE("non-positive prefix sums") {
       CHECK(non_positive_sum(nums_0, ARRAY_SIZE(nums_0)) == true);
@@ -32,5 +39,9 @@ TEST_CASE("Test week1/2 functions") {
       CHECK(non_positive_sum(nums_3, ARRAY_SIZE(nums_3)) == false);
       CHECK(non_positive_sum(nums_4, ARRAY_SIZE(nums_4)) == false);
       CHECK(non_positive_sum(nums_5, ARRAY_SIZE(nums_5)) == true);
+      CHECK(non_positive_sum(nums_6, ARRAY_SIZE(nums_6)) == false);
+      CHECK(non_positive_sum(nums_7, ARRAY_SIZE(nums_7)) == true);
+      CHECK(non_positive_sum(nums_8, ARRAY_SIZE(nums_8)) == false);
+      CHECK(non_positive_sum(nums_9, ARRAY_SIZE(nums_9)) == true);
   };
 }
