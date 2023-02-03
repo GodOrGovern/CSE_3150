69b60661a2201d2711b5f2d8e0988c436a81edff
GodOrGovern <willkeenan12@gmail.com>
2023-02-03 15:11:00 -0500

    Minor changes

--- Week2_Lab/main.cpp
+++ Week2_Lab/main.cpp
@@ -2,9 +2,13 @@
 #include "functions.h"
 
 int main(void) {
+    using namespace std;
+
+    cout << "n: (# of well-balanced) / (# total) " << endl;
     for (int n = 1; n < 20; n++) {
         double result = run_tests(n, 50000);
-        std::cout << n << ": " << result << std::endl;
+        cout << n << ": " << result << endl;
     }
+
     return 0;
 }
--- Week2_Lab/unit_tests.cpp
+++ Week2_Lab/unit_tests.cpp
@@ -13,16 +13,31 @@ int array1[10];
 int test1[] = {1,-1};
 int test2[] = {1,1,-1,-1};
 int test3[] = {1,1,1,1,1,-1,-1,-1,-1,-1};
+int test4[] = {1,-1,1,-1};
+int test5[] = {1,-1,1,-1,-1};
+int test6[] = {1};
+int test7[] = {-1};
+int test8[] = {-1,-1,1,1,1,-1};
 
 TEST_CASE("Test week 2 lab functions") {
   SUBCASE("fill_array") {
       fill_array(array1, 2);
-      CHECK(std::equal(array1, array1+2, test1));
+      CHECK(std::equal(array1, array1+ARRAY_SIZE(test1), test1));
       fill_array(array1, 4);
-      CHECK(std::equal(array1, array1+4, test2));
+      CHECK(std::equal(array1, array1+ARRAY_SIZE(test2), test2));
       fill_array(array1, 10);
-      CHECK(std::equal(array1, array1+10, test3));
+      CHECK(std::equal(array1, array1+ARRAY_SIZE(test3), test3));
   };
   SUBCASE("check_array") {
+      CHECK(check_array(test1, ARRAY_SIZE(test1)) == true);
+      CHECK(check_array(test2, ARRAY_SIZE(test2)) == true);
+      CHECK(check_array(test4, ARRAY_SIZE(test4)) == true);
+      CHECK(check_array(test5, ARRAY_SIZE(test5)) == false);
+      CHECK(check_array(test6, ARRAY_SIZE(test6)) == true);
+      CHECK(check_array(test7, ARRAY_SIZE(test7)) == true);
+      CHECK(check_array(test8, ARRAY_SIZE(test8)) == false);
+  };
+  SUBCASE("fisher_yates") {
+
   };
 }
