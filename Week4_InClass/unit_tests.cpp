359748cc45d345bdac7642a429b7ff9b99129ad5
GodOrGovern <willkeenan12@gmail.com>
2023-02-12 18:17:58 -0500

    Changes

--- Week4_InClass/unit_tests.cpp
+++ Week4_InClass/unit_tests.cpp
@@ -11,9 +11,24 @@
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 
+int length = 7;
+int array[7] = {1,2,3,4,5,6,-40};
+
 TEST_CASE("Test week 4 in class") {
-  SUBCASE("Test queue") {
-  };
   SUBCASE("Test stack") {
+      Stack stack;
+      readTextFileToStack(stack, "file.txt");
+      for (int i = length; i > 0; i--) {
+          CHECK(stack.stack.top().val == array[i-1]);
+          popFromStack(stack);
+      }
+  };
+  SUBCASE("Test queue") {
+      std::queue<int> queue;
+      readTextFileToQueue(queue, "file.txt");
+      for (int i = 0; i < length; i++) {
+          CHECK(queue.front() == array[i]);
+          popFromQueue(queue);
+      }
   };
 }
